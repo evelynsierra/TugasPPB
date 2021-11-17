64ab939297e1c23c1e1b0666a020289e718e9ded
evelynsierra <sierravelin@gmail.com>
2021-11-17 23:00:30 +0700

    akses permission write storage

--- kamera/app/src/main/java/com/example/kamera/MainActivity.java
+++ kamera/app/src/main/java/com/example/kamera/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.kamera;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -14,18 +18,24 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.text.DateFormat;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
      Button bl;
      ImageView iv;
+     String nmFile;
     private static final int kodekamera = 222;
+    private static final int MY_PERMISSIONS_REQUEST_WRITE = 223;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        askWritePermission();
         setContentView(R.layout.activity_main);
 
         bl = (Button) findViewById(R.id.button);
@@ -35,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent it = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
                 startActivityForResult(it,
                         kodekamera);
             }
@@ -45,14 +56,48 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == Activity.RESULT_OK) {
             switch(requestCode) {
-                case(kodekamera) : prosesKamera(data); break;
+                case(kodekamera) :
+                    try {
+                        prosesKamera(data);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    break;
             }
         }
     }
-    private void prosesKamera(Intent datanya ){
+    private void prosesKamera(Intent datanya) throws IOException {
         Bitmap bm;
-        bm = (Bitmap) datanya.getExtras().get("data");
+       // bm = (Bitmap) datanya.getExtras().get("data");
+        BitmapFactory.Options options;
+        options = new BitmapFactory.Options();
+        options.inSampleSize = 2;
+        bm = BitmapFactory.decodeFile(nmFile, options);
         iv.setImageBitmap(bm);
-        Toast.makeText(this, "Data telah terload ke ImageView", Toast.LENGTH_SHORT).show();
+
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+
+        bm.compress(Bitmap.CompressFormat.PNG, 100, stream);
+        byte[] byteArray = stream.toByteArray(); //convert camera photo to byte array
+
+        //save to external storage
+        File dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM);
+        File output = new File(dir, "simpan.png");
+        FileOutputStream fo = new FileOutputStream(output);
+        fo.write(byteArray);
+        fo.flush();
+        fo.close();
+
+        Toast.makeText(this, "Data telah terload ke ImageView", Toast.LENGTH_LONG).show();
+    }
+
+    private void askWritePermission() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            int cameraPermission = this.checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            if(cameraPermission != PackageManager.PERMISSION_GRANTED) {
+                this.requestPermissions(new String[]
+                        {Manifest.permission.WRITE_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_WRITE);
+            }
+        }
     }
 }
